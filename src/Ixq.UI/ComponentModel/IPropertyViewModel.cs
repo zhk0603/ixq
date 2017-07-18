06cf07692f4792b730a5c6e4df15ddcab1b4f2a6
kun.zhao <zk9474@hotmail.com>
2017-07-18 18:33:26 +0800

    完善、实现 IPropertyViewModel接口

--- src/Ixq.UI/ComponentModel/IPropertyViewModel.cs
+++ src/Ixq.UI/ComponentModel/IPropertyViewModel.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ixq.Core.Entity;
 
 namespace Ixq.UI.ComponentModel
 {
     /// <summary>
     ///     属性查看模型接口。
     /// </summary>
-    public interface IPropertyViewModel
+    public interface IPropertyViewModel : IPropertyEditModel
     {
     }
 }
