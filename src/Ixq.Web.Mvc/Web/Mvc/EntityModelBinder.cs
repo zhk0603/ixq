f166423165ea9492881b4d67ae720d5926cb0666
kun.zhao <zk9474@hotmail.com>
2017-09-15 23:27:10 +0800

    添加 EntityModelBinder.

--- src/Ixq.Web.Mvc/Web/Mvc/EntityModelBinder.cs
+++ src/Ixq.Web.Mvc/Web/Mvc/EntityModelBinder.cs
@@ -1,4 +1,5 @@
-﻿using Ixq.Core.Repository;
+﻿using Ixq.Core.Entity;
+using Ixq.Core.Repository;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,22 +10,64 @@ using System.Web.Mvc;
 
 namespace Ixq.Web.Mvc
 {
+    /// <summary>
+    ///     实体模型映射对象。
+    /// </summary>
     public class EntityModelBinder : DefaultModelBinder
     {
-        protected override object GetPropertyValue(ControllerContext controllerContext, ModelBindingContext bindingContext, PropertyDescriptor propertyDescriptor, IModelBinder propertyBinder)
+        /// <summary>
+        ///     在更新模型后调用。
+        /// </summary>
+        /// <param name="controllerContext">运行控制器的上下文。上下文信息包括控制器、HTTP 内容、请求上下文和路由数据。</param>
+        /// <param name="bindingContext">绑定模型的上下文。上下文包含模型对象、模型名称、模型类型、属性筛选器和值提供程序等信息。</param>
+        protected override void OnModelUpdated(ControllerContext controllerContext, ModelBindingContext bindingContext)
         {
-            return base.GetPropertyValue(controllerContext, bindingContext, propertyDescriptor, propertyBinder);
+            base.OnModelUpdated(controllerContext, bindingContext);
+            ModelStateUpdate(controllerContext, bindingContext);
         }
-        protected override void BindProperty(ControllerContext controllerContext, ModelBindingContext bindingContext, PropertyDescriptor propertyDescriptor)
-        {
-            // need to skip properties that aren't part of the request, else we might hit a StackOverflowException
-            string fullPropertyKey = CreateSubPropertyName(bindingContext.ModelName, propertyDescriptor.Name);
-            if (!bindingContext.ValueProvider.ContainsPrefix(fullPropertyKey))
-            {
-                return;
-            }
 
-            base.BindProperty(controllerContext, bindingContext, propertyDescriptor);
+        /// <summary>
+        ///     更新模型状态。
+        /// </summary>
+        /// <param name="controllerContext">运行控制器的上下文。上下文信息包括控制器、HTTP 内容、请求上下文和路由数据。</param>
+        /// <param name="bindingContext">绑定模型的上下文。上下文包含模型对象、模型名称、模型类型、属性筛选器和值提供程序等信息。</param>
+        protected virtual void ModelStateUpdate(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            foreach (var modelStateDic in bindingContext.ModelState)
+            {
+                OnModelStateUpdate(controllerContext, bindingContext, modelStateDic);
+            }
+        }
+        protected virtual void OnModelStateUpdate(ControllerContext controllerContext, ModelBindingContext bindingContext, KeyValuePair<string, ModelState> modelStateDic)
+        {
+            if (modelStateDic.Value.Errors.Count > 0)
+            {
+                var entityControllerDescriptor = controllerContext.Controller as IEntityControllerDescriptor;
+                if (entityControllerDescriptor != null)
+                {
+                    UpdateModelStateWithEntityMetadata(entityControllerDescriptor.EntityMetadata, bindingContext, modelStateDic);
+                }
+            }
+        }
+
+        /// <summary>
+        ///     使用实体元数据更新模型状态。
+        /// </summary>
+        /// <param name="metadata">实体元数据。</param>
+        /// <param name="bindingContext">绑定模型的上下文。上下文包含模型对象、模型名称、模型类型、属性筛选器和值提供程序等信息。</param>
+        /// <param name="modelStateDic">模型状态键值对。在模型绑定时发生错误的模型状态。</param>
+        protected virtual void UpdateModelStateWithEntityMetadata(IEntityMetadata entityMetadata, ModelBindingContext bindingContext, KeyValuePair<string, ModelState> modelStateDic)
+        {
+            // 当前授权用户没有此属性的编辑权限时，清楚模型错误信息。
+            if (!HasEditProperty(entityMetadata.EditPropertyMetadatas, modelStateDic.Key))
+            {
+                modelStateDic.Value.Errors.Clear();
+            }
+        }
+
+        private bool HasEditProperty(IEntityPropertyMetadata[] metadata, string name)
+        {
+            return metadata.Any(x => x.PropertyName == name);
         }
     }
 }
