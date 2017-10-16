aaf3170175ff7ba2ec0f9ed589f62e5af082263d
kun.zhao <zk9474@hotmail.com>
2017-10-16 22:39:30 +0800

    添加 ISerializableService，未实现。

---
+++ src/Ixq.Redis/ISerializableService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ixq.Redis
+{
+    /// <summary>
+    ///     序列化服务。
+    /// </summary>
+    public interface ISerializableService
+    {
+        object Deserialize(byte[] data);
+        byte[] Serialize(object obj);
+    }
+}
---
+++ src/Ixq.Redis/SerializableService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ixq.Redis
+{
+    public class SerializableService : ISerializableService
+    {
+        public object Deserialize(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Serialize(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
